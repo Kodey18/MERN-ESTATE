fa45cc9bb2cb400f02d38be6ee9b9b23e7481d49
karan adwani <karanadwani.official@gmail.com>
2023-10-06 01:34:28 +0530

    handlechange function reated

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value,
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className='max-w-lg mx-auto p-7'>
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up!</h1>
@@ -11,18 +24,21 @@ const Signup = () => {
           placeholder="username"
           className="border rounded-lg p-3"
           id="username"
+          onChange={(e) => handleChange(e)}
         />
         <input
           type="text"
           placeholder="email"
           className="border rounded-lg p-3"
           id="email"
+          onChange={(e) => handleChange(e)}
         />
         <input
           type="password"
           placeholder="password"
           className="border rounded-lg p-3"
           id='password'
+          onChange={(e) => handleChange(e)}
         />
         <button 
           className='bg-slate-800 rounded-lg text-white p-3 hover:opacity-95 uppercase text-lg disabled:opacity-80'
