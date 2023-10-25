86b4e849d55f1c8eed465eff1911ba134a0f7182
karan adwani <karanadwani.official@gmail.com>
2023-10-25 16:55:52 +0530

    user-signOut reducers added

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -45,6 +45,18 @@ const userSlice = createSlice({
         deleteUserFailure: (state, action) => {
             state.errors = action.payload;
             state.loading = false;
+        },
+        signOutUserStart: (state) => {
+            state.loading = true;
+        },
+        signOutUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.errors = null;
+        },
+        signOutUserFailure: (state, action) => {
+            state.errors = action.payload;
+            state.loading = false;
         }
     }
 });
