5b68847b9a66d27dd363daafb2c8d6275b2851cd
karan adwani <karanadwani.official@gmail.com>
2023-10-25 23:10:50 +0530

    create ground controller created

--- backend/controllers/campGroundController.js
+++ backend/controllers/campGroundController.js
@@ -7,6 +7,46 @@ Route: POST /api/ground/create
 Access: Private (Token)
 */
 const createGrounds = asyncHnadler( async(req, res) => {
+    const {
+        name,
+        description,
+        address,
+        regularPrice,
+        discountedPrice,
+        offer,
+        imageUrls,
+        capacity,
+        sites,
+        amenities,
+        activities,
+        pets,
+        userRef,
+    } = req.body; // Assuming you're sending data in the request body
+
+    if(!name ||
+        !description ||
+        !address ||
+        !regularPrice ||
+        !discountedPrice ||
+        !offer ||
+        !imageUrls ||
+        !capacity ||
+        !sites ||
+        !amenities ||
+        !activities ||
+        !pets
+    ){
+        return res.status(400).json({msg:'Please fill all fields.'});
+    }
+
+    try{
+        const savedGround = await Ground.create(req.body);
+        return res.status(200).json(savedGround);
+    }catch(err){
+        const error = new Error(`Error creatin a ground ${err}`);
+        error.statusCode = 400;
+        throw error;
+    }
 });
 
 module.exports = {
