78e693b6b635b4df0d0a1c176501c8d721060b22
karan adwani <karanadwani.official@gmail.com>
2023-10-27 07:30:27 +0530

    ground model updated

--- backend/models/groundModel.js
+++ backend/models/groundModel.js
@@ -1,63 +1,85 @@
 const mongoose = require('mongoose');
 
-const groundSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const groundSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    pets: {
+      type: Boolean,
+      required: true,
+    },
+    tours: {
+      type: Boolean,
+      required: true,
+    },
+    rentalEquip: {
+      type: Boolean,
+      required: true,
+    },
+    foodService: {
+      type: Boolean,
+      required: true,
+    },
+    Accommodations: {
+      type: String,
+      required: true,
+    },
+    intake: {
+      type: Number,
+      required: true,
+    },
+    sites: {
+      type: Number,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountedPrice: {
+      type: Number,
+      default: null,
+      required: true,
+    },
+    amenities: {
+      type: [String],
+      required: true,
+    },
+    activities: {
+      type: [String],
+      required: true,
+    },
+    lat: {
+      type :Number,
+      required:true,
+    },
+    lng: {
+      type :Number,
+      required:true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  regularPrice: {
-    type: Number,
-    required: true,
-  },
-  discountedPrice: {
-    type: Number,
-    default: null,
-    required: true,
-  },
-  offer:{
-    type:Boolean,
-    required: true,
-  },
-  imageUrls: {
-    type: Array,
-    required: true,
-  },
-  capacity: {
-    type: Number,
-    required: true,
-  },
-  sites: {
-    type: Number,
-    required: true,
-  },
-  amenities: {
-    type: [String],
-    required: true,
-  },
-  activities: {
-    type: [String],
-    required: true,
-  },
-  pets: {
-    type: Boolean,
-    required: true,
-  },
-  userRef: {
-    type: String,
-    required: true,
-  },
-},
-{ 
-    timestamps: true 
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Ground = mongoose.model('Ground', groundSchema);
 
